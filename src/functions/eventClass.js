f21cc5918fa7acb97b1edef6509956d9fa2ee9e4
Lovalova322 <aidosov.damir@gmail.com>
2021-11-13 17:14:30 +0300

    updated

--- src/functions/eventClass.js
+++ src/functions/eventClass.js
@@ -2,7 +2,7 @@ export default (function() { //generating shop event (removing, placing products
     var nextId = 111128;
   
      return function shopEvent(productName, actionType, count, sessionId) {
-        this.timestamp = Date.now()/1000,
+        this.timestamp = parseInt(Date.now()/1000),
         this.productName = productName,
         this.id= nextId++,
         this.type= actionType,
--- src/functions/trunstileEventClass.js
+++ src/functions/trunstileEventClass.js
@@ -2,7 +2,7 @@ export default (function() { //generating exit/entry to shop event
     var nextId = 4757;
   
      return function shopEvent(actionType, sessionId) {
-        this.timestamp = Date.now()/1000,
+        this.timestamp = parseInt(Date.now()/1000),
         this.id= nextId++,
         this.type= actionType,
         this.sessionId= sessionId
